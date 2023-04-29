7ec8ccea186c23984df76d3de04dc1b4acaaa36a
Michael-Harber <129912256+Michael-Harber@users.noreply.github.com>
2023-04-28 21:12:28 -0700

    Add files via upload

---
+++ week3/src/GallonsLiters.java
@@ -0,0 +1,37 @@
+public class GallonsLiters {
+
+    public static void main(String[] args) {
+
+        double gal, lit;
+        int counter = 1;
+
+        for (gal = 1; gal <= 100; gal = gal + 3) {
+            lit = gal * 3.785;
+
+
+
+                if (gal <= 1) {
+                    System.out.printf("%.2f", lit);
+                    System.out.println(" Liters = " + gal + " Gallon");
+                } else {
+                    System.out.printf("%.2f", lit);
+                    System.out.println(" Liters = " + gal + " Gallons");
+                    counter += 1;
+
+                 if (counter > 5){
+                     System.out.println( );
+                     counter = 0;
+
+                 }
+
+                }
+
+
+            }
+
+        }
+    }
+
+
+
+
